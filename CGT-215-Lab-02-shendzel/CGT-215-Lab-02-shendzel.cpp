85d944f01359ce1cfd9b6f285ec783aefd38f1db
Sebastian <sebastian.hendzel@gmail.com>
2026-09-01 12:33:29 -0400

    Added a new equation

--- CGT-215-Lab-02-shendzel/CGT-215-Lab-02-shendzel.cpp
+++ CGT-215-Lab-02-shendzel/CGT-215-Lab-02-shendzel.cpp
@@ -6,7 +6,8 @@ int main()
 	float A;
 	float B;
 	float X;
-	cout << "B = (A + 4) + 1 " << endl;
+	float C;
+	cout << "B = (A + 4) + 1 " << endl; 
 	
 	cout << "Please enter a value for A: ";
 	cin >> A;
@@ -15,4 +16,28 @@ int main()
 
 	B = (A + 4) + 1;
 	cout << "B= " << B << endl;
+
+	//same thing but with 3 variables
+
+	//float A; thought it might redo the variable and get rid of the previously mentioned one, but instead just left it
+	//float B;
+	//float X;
+
+	cout << "A = 4C + B" << endl;
+	cout << "solve for C" << endl;
+
+	cout << "please enter a value for A: ";
+	cin >> A;
+	cout << "A = " << A << endl;
+
+	cout << "Now enter a value for B: ";
+	cin >> B;
+	cout << "B = " << B << endl;
+
+	cout << A << " = 4" << "C" << " + " << B << endl;
+	
+	C = (A - B) / 4; //I realize you have to break the equation down to whatever the varaible is equal to because the code wont do it.
+		
+	cout << "C = " << C << endl;
+
 }
